535ae460f62ce3c60234dcbad08d55527fb33edd
Rasskopovaa <raskopovaa@gmail.com>
2020-11-08 03:08:55 +0700

    the variant of bot which can parse some sites

--- src/main/java/parser/Facts.java
+++ src/main/java/parser/Facts.java
@@ -9,18 +9,26 @@ import java.util.Random;
 
 
 public class Facts {
-    public static void main(String[] args) throws IOException {
+    public String getFact() throws IOException {
         String url = "https://zen.yandex.ru/media/id/5cd59c2cf9616800b423d410/samorazvitie-interesnye-fakty-obo-vsem-na-svete-50-faktov-5cd6b792c9c89500afe92211";
         Document document = Jsoup.connect(url).get();
         Elements elements = document.select("blockquote:not(0)");
         int numberOfElements = 2;
+        String str="";
         Random random = new Random();
         for (int i = 1; i < numberOfElements; i++) {
             int randomIndex = random.nextInt(elements.size());
-            System.out.println(elements.get(randomIndex).text());
-            //elements.remove(randomIndex);
+             str =elements.get(randomIndex).text();
         }
-    }
+        return str;
 
     }
+
+   public static void main(String[] args) throws IOException {
+        Facts facts = new Facts();
+       System.out.println(facts.getFact());
+        //elements.remove(randomIndex);
+    }
+    }
+
 
--- src/main/java/telegram/bot/Bot.java
+++ src/main/java/telegram/bot/Bot.java
@@ -1,6 +1,7 @@
 package telegram.bot;
 
 
+import com.vdurmont.emoji.EmojiParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,17 +10,36 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import parser.Afirmations;
+import parser.Facts;
+import parser.Weather;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class Bot  extends TelegramLongPollingBot {
     private static final Logger logger = LoggerFactory.getLogger(Bot.class);
+    String strErr="Введена неверная команда.Пожалуйста, выберите действие.";
 
     @Value("${bot.name}")
     private String botUsername;
 
     @Value("${bot.token}")
     private String botToken;
+
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    public String getBotToken() {
+        return botToken;
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -29,28 +49,64 @@ public class Bot  extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-    }*/
+    }
+    */
         if (update.hasMessage()) {
             Message message = update.getMessage();
             SendMessage response = new SendMessage();
             Long chatId = message.getChatId();
             response.setChatId(chatId);
             String text = message.getText();
-            response.setText(text);
-            try {
+            setButtons(response);
+
+            try{
+            if(text.equals("Аффирмация")){
+                Afirmations afirmations = new Afirmations();
+                response.setText(afirmations.getAfirmation());
+            }
+            if(text.equals("Погода")){
+                Weather weather = new Weather();
+                response.setText(weather.getWeather());
+            }
+            if(text.equals("Факт")){
+                Facts facts = new Facts();
+                response.setText(facts.getFact());
+            }
+           if(text.equals("/start" )){
+               response.setText(text);
+           }
+           else if (!text.equals("Факт") && !text.equals("Погода")&& !text.equals("Аффирмация")){
+               response.setText(strErr);
+           }
                 execute(response);
                 logger.info("Sent message \"{}\" to {}", text, chatId);
             } catch (TelegramApiException e) {
                 logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+
+            }
+            catch (IOException ex){
+                logger.error(ex.getMessage());
             }
         }
     }
-    public String getBotUsername() {
-        return botUsername;
+        public synchronized void setButtons (SendMessage sendMessage){
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+            replyKeyboardMarkup.setSelective(true);
+            replyKeyboardMarkup.setResizeKeyboard(true);
+            replyKeyboardMarkup.setOneTimeKeyboard(false);
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            KeyboardRow keyboardFirstRow = new KeyboardRow();
+            keyboardFirstRow.add(new KeyboardButton("Погода"));
+            KeyboardRow keyboardSecondRow = new KeyboardRow();
+            keyboardSecondRow.add(new KeyboardButton("Аффирмация"));
+            keyboardSecondRow.add(new KeyboardButton("Факт"));
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+
     }
 
-    public String getBotToken() {
-        return botToken;
-    }
 }
+
 
